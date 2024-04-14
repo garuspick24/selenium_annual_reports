3da6616c17ad5f7ec929f8636c49a6646c13ce3e
garuspick24 <122586302+garuspick24@users.noreply.github.com>
2024-04-14 19:12:19 +0300

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,21 +12,16 @@ options.add_experimental_option('detach', True)
 browser = webdriver.Chrome(options=options)
 
 
-def download_pdf(company_name: str, rank: int, year: int = 2023):
+def download_pdf(company_name: str, rank: str, year: int = 2023):
     name_for_url = company_name.replace(" ", "+")
     elements = browser.get(f"https://www.google.com/search?q={name_for_url}+annual+report+{year}+filetype:pdf+&start=0")
 
-    # Очікування появи посилання
     first_result_link = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'div.g:nth-child(1) a'))
     )
 
-    # Клік на посилання
     first_result_link.click()
-
-    # Перехід на нову вкладку
-    time.sleep(10)
-    browser.switch_to.window(browser.window_handles[-1])
+    time.sleep(4)
 
     pdf_link = browser.current_url  # Отримати поточну URL-адресу вікна з PDF-файлом
 
@@ -36,6 +32,21 @@ def download_pdf(company_name: str, rank: int, year: int = 2023):
         print("PDF-файл завантажено успішно.")
     else:
         print("Помилка завантаження PDF-файлу.")
+    time.sleep(10)
 
 
-download_pdf("JPMorgan Chase", 1, 2023)
+def download_multiple_pdfs(file_name: str):
+    with open(file_name, "r") as json_file:
+        data = json.load(json_file)
+        for element in data:
+            try:
+                download_pdf(element['name'], element['rank'])
+                print(f"Завантажено {element['rank']}. {element['name']}")
+                time.sleep(10)
+            except Exception as e:
+                with open("errors.txt", "a") as errors_file:
+                    errors_file.write(f"{element['rank']}. {element['name']}: {str(e)}\n")
+                print(f"Помилка завантаження {element['rank']}. {element['name']}: {str(e)}")
+
+
+download_multiple_pdfs("forbes.json")
